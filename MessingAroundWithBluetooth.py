820c039cb116bb7e484b15364463ad33dca48a0d
Sophia Seitz <phia.seitz@gmail.com>
2014-03-07 17:24:24 -0500

    Changed the program to print out the address and name of each discovered device.

--- MessingAroundWithBluetooth.py
+++ MessingAroundWithBluetooth.py
@@ -1,10 +1,17 @@
+# file: inquiry.py
+# auth: Albert Huang <albert@csail.mit.edu>
+# desc: performs a simple device inquiry followed by a remote name request of
+#       each discovered device
+# $Id: inquiry.py 401 2006-05-05 19:07:48Z albert $
+#
+
 import bluetooth
-#Note that this will only run with python 2.7, not the newer version for some reason
 
-print('now beginning search')
-nearby_devices = bluetooth.discover_devices()
+print("performing inquiry...")
 
-print ('search finished')
-for bdaddr in nearby_devices:
-	print (bdaddr)
-	print (bluetooth.lookup_name(bdaddr))
+nearby_devices = bluetooth.discover_devices(lookup_names = True)
+
+print("found %d devices" % len(nearby_devices))
+
+for addr, name in nearby_devices:
+    print("  %s - %s" % (addr, name))
