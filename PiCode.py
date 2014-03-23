e3c9147eb17b98f7dbcfd8ec05e1f6be426099e7
Sophia Seitz <phia.seitz@gmail.com>
2014-03-23 17:12:58 -0400

    hopefully fixed a dumb mistake

--- PiCode.py
+++ PiCode.py
@@ -6,7 +6,6 @@
 
 from bluetooth import *
 import sys
-import string
 
 if sys.version < '3':
     input = raw_input
@@ -34,7 +33,7 @@ print("Accepted connection from ", client_info)
 try:
     while True:
         data = client_sock.recv(1024)
-        if string.lower(data) == "end": break
+        if data.lower() == "end": break
         print("received [%s]" % data)
         #Process data here, and send stuff to the arduino
         if data == "1":
